a1c3179371309f0363af3c5911e1dc216307ab1b
Chris Reichert <chris.a.reichert@gmail.com>
2022-01-07 16:01:21 -0600

    setup complete from memory

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView,ListView,UpdateView,DeleteView,CreateView, DetailView
+from .models import Snack
 
-# Create your views here.
+
+class SnackListView(ListView):
+    template_name = 'snack_list_view.html'
+    model = Snack
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail_view.html'
+
+class SnackCreateView(CreateView):
+    template_name = 'snack_create_view.html'
+    model = Snack
+    fields = ['title','purchaser','description',]
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack_update_view.html'
+    model = Snack
+    fields = ['title','purchaser','description',]
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack_delete_view.html'
