8365dbc26c780e7d2498a25b999acd747d611092
Chris Reichert <chris.a.reichert@gmail.com>
2022-01-16 17:09:29 -0600

    CRUD works

--- snacks/views.py
+++ snacks/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView,ListView,UpdateView,DeleteView,CreateView, DetailView
+from django.urls import reverse_lazy
 from .models import Snack
 
 
@@ -13,12 +14,14 @@ class SnackDetailView(DetailView):
 class SnackCreateView(CreateView):
     template_name = 'snack_create_view.html'
     model = Snack
-    fields = ['title','purchaser','description',]
+    fields = ["title","purchaser","description",]
 
 class SnackUpdateView(UpdateView):
     template_name = 'snack_update_view.html'
     model = Snack
-    fields = ['title','purchaser','description',]
+    fields = ['title','purchaser','description']
 
 class SnackDeleteView(DeleteView):
     template_name = 'snack_delete_view.html'
+    model = Snack
+    success_url = reverse_lazy('snack_list')
